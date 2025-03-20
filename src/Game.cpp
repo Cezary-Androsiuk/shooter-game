7ac725d1883e56885d4180c641105f253ea4e4ee
Cezary Androsiuk PC <czandros@wp.pl>
2025-03-20 17:12:37 +0100

    created Game class template

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,16 +2,83 @@
 
 void Game::initWindow()
 {
-    m_window = new sf::RenderWindow();
+    sf::VideoMode vm = DEBUG_VIEW ? sf::VideoMode(400, 200) : sf::VideoMode::getDesktopMode();
+    // m_contextSettings
+    m_window = new sf::RenderWindow(vm, "Edu Game", DEBUG_VIEW ? sf::Style::Default : sf::Style::Fullscreen, m_contextSettings);
+    // m_window->setFramerateLimit(240);
 }
 
 Game::Game()
 {
+    this->initWindow();
+}
 
+Game::~Game()
+{
+    delete m_window;
+}
+
+void Game::updateCurrentrTick()
+{
+    ++m_currentTick;
+}
+
+void Game::updateDeltaTime()
+{
+    m_dt = m_deltaTimeClock.restart().asSeconds();
+}
+
+void Game::pollEvent()
+{
+    while(m_window->pollEvent(m_currentEvent)){
+        switch (m_currentEvent.type){
+        case sf::Event::Closed:
+            m_window->close();
+            break;
+        case sf::Event::KeyPressed:
+            if(m_currentEvent.key.code == sf::Keyboard::Escape)
+                m_window->close();
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::update()
+{
+    if(m_currentTick % 100 == 1)
+    {
+        int fps = 1.f/m_dt;
+        printf("\r                              \r");
+        printf("frame: %lld, fps: %d", m_currentTick, fps);
+        fflush(stdout);
+    }
+}
+
+void Game::render()
+{
+    m_window->clear(BACKGROUND_SF_COLOR);
+
+    m_window->display();
+}
+
+bool Game::running() const noexcept
+{
+    return m_window->isOpen();
 }
 
 void Game::play()
 {
     Game game;
-    // while(game.)
+    while(game.running())
+    {
+        game.updateCurrentrTick();
+        game.updateDeltaTime();
+
+        game.pollEvent();
+        game.update();
+        game.render();
+    }
+    printf("\n");
 }
--- src/Game.h
+++ src/Game.h
@@ -3,20 +3,44 @@
 
 #include <SFML/Graphics.hpp>
 
+#define DEBUG_VIEW true // makes window smaller
+#define BACKGROUND_SF_COLOR sf::Color(30, 30, 30)
+
 class Game
 {
+    /* INITIALIZE */
     void initWindow();
 
     Game();
+    ~Game();
 
+    /* OTHER */
+    void updateCurrentrTick();
+    void updateDeltaTime();
 
-public: static void play();
+    /* EVENTS */
+    void pollEvent();
+
+    /* UPDATE */
+    void update();
+
+    /* RENDER */
+    void render();
+
+    inline bool running() const noexcept;
+
+public:
+    static void play();
+
 private:
-    bool running() const;
-
-
     sf::RenderWindow* m_window;
+    sf::ContextSettings m_contextSettings;
     sf::Event m_currentEvent;
+
+    sf::Clock m_deltaTimeClock;
+    float m_dt;
+
+    size_t m_currentTick;
 };
 
 #endif // GAME_H
