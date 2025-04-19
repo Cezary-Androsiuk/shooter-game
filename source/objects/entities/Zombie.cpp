080330399491976b419f43393a9aac9657d451f2
Cezary Androsiuk PC <czandros@wp.pl>
2025-04-19 02:36:55 +0200

    implemented zombie

--- source/objects/entities/Zombie.cpp
+++ source/objects/entities/Zombie.cpp
@@ -1,3 +1,74 @@
 #include "Zombie.h"
 
-Zombie::Zombie() {}
+void Zombie::initData()
+{
+    m_movementSpeedAddons.msDefault = Data::Enemy::Zombie::getMovementSpeed();
+    m_health = Data::Enemy::Zombie::getHealth();
+    m_damage = Data::Enemy::Zombie::getDamage();
+
+    /// Size
+    m_size.x = 50.f;
+    m_size.y = 50.f;
+}
+
+void Zombie::initBody()
+{
+    m_body.bounds.setPosition(m_position);
+    m_body.bounds.setFillColor(sf::Color(180, 40, 40));
+    m_body.bounds.setSize(m_size);
+
+    m_body.boundsShadow.setPosition({m_position.x-5, m_position.y-5});
+    m_body.boundsShadow.setFillColor(sf::Color(130, 20, 20, 180));
+    m_body.boundsShadow.setSize({m_size.x+10, m_size.y+10});
+}
+
+Zombie::Zombie()
+    : Enemy()
+{
+
+}
+
+void Zombie::limitZombieMovementToMap()
+{
+    FloatRectEdges playerEdges(m_position.x, m_position.y, m_position.x + m_size.x, m_position.y + m_size.y);
+    float windowSizeX = static_cast<float>(m_map->getMapSize().x);
+    float windowSizeY = static_cast<float>(m_map->getMapSize().y);
+    FloatRectEdges windowEgdes(0.f, 0.f, windowSizeX, windowSizeY);
+
+    for(Obstacle *obstacle : m_map->getObstacles())
+        this->preventMoveThatEnterBounds(playerEdges, obstacle->getBounds());
+}
+
+void Zombie::updateBody()
+{
+    m_body.bounds.setPosition(m_position);
+    m_body.boundsShadow.setPosition({m_position.x-5, m_position.y-5});
+}
+
+void Zombie::init()
+{
+    Enemy::init();
+
+    this->initData();
+    this->initBody();
+}
+
+void Zombie::pollEvent(const sf::Event &event)
+{
+    Enemy::pollEvent(event);
+}
+
+void Zombie::update()
+{
+    Enemy::update();
+
+    this->performMoveTowardsPlayer();
+    this->limitZombieMovementToMap();
+
+    this->updateBody();
+}
+
+void Zombie::render(sf::RenderTarget *target)
+{
+
+}
--- source/objects/entities/Zombie.h
+++ source/objects/entities/Zombie.h
@@ -1,10 +1,35 @@
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
 
-class Zombie
+#include "utils/Data.h"
+#include "objects/entities/Enemy.h"
+
+class Zombie : public Enemy
 {
+    /* INITIALIZE */
+    void initData();
+    void initBody();
+
 public:
     Zombie();
+
+private:
+    /* UPDATE */
+    void limitZombieMovementToMap();
+    void updateBody();
+
+public:
+    void init() override;
+
+    void pollEvent(const sf::Event &event) override;
+    void update() override;
+    void render(sf::RenderTarget *target) override;
+
+private:
+    struct{
+        sf::RectangleShape bounds;
+        sf::RectangleShape boundsShadow;
+    } m_body;
 };
 
 #endif // ZOMBIE_H
